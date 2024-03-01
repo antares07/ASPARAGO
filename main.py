51af0fafe37af10a23002a49e9c4b53c9703fe25
antares07 <lpaglia98@gmail.com>
2024-03-01 13:05:33 +0000

    Removed older version

--- main.py
+++
@@ -1,9 +0,0 @@
-from functions import generate_hdf5_dataset
-import time
-
-if __name__ == '__main__':
-    print('Starting generation...')
-    start = time.time()
-    generate_hdf5_dataset(1, '/home/pagliaro/SharedDataset')
-    end = time.time()
-    print(f'Process ends in {(end-start)/60} minutes')
